7c6872d5eb5442bf364bb131bd157f6579a8b1b9
Sand Of Vega <sandofvega@gmail.com>
2018-10-10 20:35:00 +0600

    envoy setup done

---
+++ Envoy.blade.php
@@ -0,0 +1,63 @@
+@servers(['sov' => ['yasitijb@server243.web-hosting.com -p21098']])
+
+@story('deploy')
+
+    introduction
+
+    git
+
+    assets
+
+    composer
+
+    conclusion
+
+@endstory
+
+@task('introduction')
+
+    cd sandofvega.com
+
+    php artisan down
+
+    cd ../public_html
+
+    rm -Rf css fonts img js error_log
+
+    cd ../sandofvega.com
+
+@endtask
+
+@task('git')
+
+    git pull origin master
+
+@endtask
+
+@task('assets')
+
+    cd public
+
+    mv css fonts img js ../../public_html/
+
+    cd ..
+
+    rm -Rf public
+
+@endtask
+
+@task('composer')
+
+    composer install
+
+@endtask
+
+@task('conclusion')
+
+    php artisan up
+
+    php artisan config:cache
+
+    php artisan route:cache
+
+@endtask
---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<title>Site Maintenance - Sand Of Vega</title>
+<link rel="icon" href="favicon.png">
+<style>
+    body { text-align: center; padding: 150px; }
+    h1 { font-size: 50px; }
+    body { font: 20px Helvetica, sans-serif; color: #333; }
+    article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+    a { color: #1abc9c; text-decoration: none; }
+    a:hover { color: #333; text-decoration: none; }
+</style>
+
+<article>
+    <h1>We&rsquo;ll be back soon!</h1>
+    <div>
+        <p>Sorry for the inconvenience but I am performing some maintenance at the moment. If you need to you can always <a href="mailto:yasin@sandofvega.com">email me</a>, otherwise I will be back online shortly!</p>
+        <p>&mdash; Sand Of Vega</p>
+    </div>
+</article>
